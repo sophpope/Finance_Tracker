66495f0ac385d713dc7d71cb9f0051aedd4c9994
Sophie Pope <sophiepope393@gmail.com>
2026-06-07 16:09:18 +0100

    Setting up the database connections to send and recieve using queries

--- db_utils.py
+++ db_utils.py
@@ -3,6 +3,7 @@ from not_for_git import YOUR_PASSWORD
 
 
 class Database:
+    #database connection
     def __init__(self):
         self.conn = psycopg2.connect(
             dbname = 'finance_tracker',
@@ -12,6 +13,25 @@ class Database:
             port = '5432'
         )
         
+        #returns a cursor object, you can use this to perform queries 
         self.cursor = self.conn.cursor()
 
+    #executing queries 
+    def execute(self, query, values = None):
+        self.cursor.execute(query, values)
+        self.conn.commit()
 
+    #retrieving multiple records from the database
+    def fetch_all(self, query, values = None):
+        self.cursor.execute(query, values)
+        return self.cursor.fetchall()
+    
+    #retrieving single values or a row from the database
+    def fetch_one(self, query, values = None):
+        self.cursor.execute(query, values)
+        return self.cursor.fetchone()
+    
+    #closing the database connection
+    def close(self):
+        self.cursor.close()
+        self.conn.close()
