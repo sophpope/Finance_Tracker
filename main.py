f0137a81e13bf4f5fc097395777a82bdd84ecf00
Sophie Pope <sophiepope393@gmail.com>
2026-06-12 11:29:40 +0100

    Error Handling for main loop

--- main.py
+++ main.py
@@ -15,63 +15,73 @@ def main():
     budget = Budget(db)
 
     while True:
-        print("\n Finance Tracker Menu:")
-        print("1. Add Expense")
-        print("2. Remove Expense")
-        print("3. View Expenses")
-        print("4. Add Income")
-        print("5. Remove Income")
-        print("6. View Income")
-        print("7. View Monthly Summary")
-        print("8. View Spending by Category")
-        print("9. Add Budget")
-        print("10. Remove Budget")
-        print("11. Monthly Budget Overview")
-        print("12. Exit")
+        try:
+            print("\n Finance Tracker Menu:")
+            print("1. Add Expense")
+            print("2. Remove Expense")
+            print("3. View Expenses")
+            print("4. Add Income")
+            print("5. Remove Income")
+            print("6. View Income")
+            print("7. View Monthly Summary")
+            print("8. View Spending by Category")
+            print("9. Add Budget")
+            print("10. Remove Budget")
+            print("11. Monthly Budget Overview")
+            print("12. Exit")
 
-        choice = input("Enter your choice (1-8): ")
+            choice = int(input("Enter your choice (1-12): "))
 
-        if choice == '1':
-            expenses.add_expense()
+            #change this is more options added to the list!
+            if choice <= 1 or choice >= 12:
+                print("Please enter a choice between 1 - 12")
+                time.sleep(2)
+                continue
 
-        elif choice == '2':
-            expenses.remove_expense()
+            if choice == '1':
+                expenses.add_expense()
 
-        elif choice == '3':
-            expenses.view_expenses()
+            elif choice == '2':
+                expenses.remove_expense()
 
-        elif choice == '4':
-            income.add_income()
+            elif choice == '3':
+                expenses.view_expenses()
 
-        elif choice == '5':
-            income.remove_income()
+            elif choice == '4':
+                income.add_income()
 
-        elif choice == '6':
-            income.view_income()
+            elif choice == '5':
+                income.remove_income()
 
-        elif choice == '7':
-            finance_report.monthly_summary()
+            elif choice == '6':
+                income.view_income()
 
-        elif choice == '8':
-            finance_report.category_spending()
+            elif choice == '7':
+                finance_report.monthly_summary()
 
-        elif choice == '9':
-            budget.add_budget()
+            elif choice == '8':
+                finance_report.category_spending()
 
-        elif choice == '10':
-            budget.remove_budget()
+            elif choice == '9':
+                budget.add_budget()
 
-        elif choice == '11':
-            budget.view_monthly_budget()
+            elif choice == '10':
+                budget.remove_budget()
 
-        elif choice == '12':
-            print("Goodbye!")
-            break
+            elif choice == '11':
+                budget.view_monthly_budget()
 
-        else:
-            print("Invalid option. Please try again.")
+            elif choice == '12':
+                print("Goodbye!")
+                break
 
-        time.sleep(2)
+            else:
+                print("Invalid option. Please try again.")
+
+            time.sleep(2)
+        
+        except ValueError:
+            print("Please enter a number between 1-12")
         
 
 if __name__ == "__main__":
